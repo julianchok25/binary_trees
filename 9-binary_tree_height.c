ebf56b128480494536ff2352a001a0fabf50d54c
cris-mur <yomy98@gmail.com>
2020-03-10 16:48:24 -0500

    [add task 9 and 10]

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - count a height of a binary tree
+ * @tree: input entire tree
+ * Return: height of a binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int h = 0;
+	if (!tree)
+		return (0);
+	if (tree->left)
+	{
+		h++;
+		binary_tree_height(tree->left);
+	}
+	if (tree->right)
+	{
+		h++;
+		binary_tree_height(tree->right);
+	}
+	return (h);
+}
