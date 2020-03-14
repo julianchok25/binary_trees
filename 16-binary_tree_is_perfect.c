286aae5c0684aa59743ccbfcfdfd7a2fb9a8f8b8
Julian Villegas <julian.villegasplus@gmail.com>
2020-03-14 00:59:25 +0000

    [FIX]16-binary_tree_perfect: Adding prototypes to the same file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,6 +18,45 @@ long int _pow(int base, int exp)
 }
 
 /**
+ * binary_tree_height - count a height of a binary tree
+ * @tree: input entire tree
+ * Return: height of a binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_R = 0, h_L = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+	{
+		h_L = binary_tree_height(tree->left);
+		h_R = binary_tree_height(tree->right);
+		return (1 + (h_L > h_R ? h_L : h_R));
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: the size of the tree
+ * If tree is NULL, the function must return 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t num_nodes = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left != NULL)
+		num_nodes += binary_tree_size(tree->left);
+	if (tree->right != NULL)
+		num_nodes += binary_tree_size(tree->right);
+	return (num_nodes + 1);
+}
+
+/**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect
  * @tree: pointer to root node of the tree to check
  * Return: 1 if tree is perfect,
